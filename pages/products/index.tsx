6f7798ac3be5055d960caceaf4d8d06d44428bee
FrancoG21 <fgimenez029@gmail.com>
2023-05-02 19:05:05 -0300

    style: cards products

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,60 +1,62 @@
 import Layout from "@/components/layouts/layout";
+import db from "@/lib/db";
+import Product from "@/models/Product";
 import React from "react";
+import {IProduct} from '../../interfaces/IProduct';
+import {BsSuitHeart, BsSuitHeartFill} from 'react-icons/bs';
+import {FaShoppingCart} from 'react-icons/fa';
 
-export default function ProductsScreen() {
+export default function ProductsScreen({products }: any  ) {
+
+  console.log(products);
   return (
     <Layout>
-      <div className="h-full w-full">
+      <div className="w-full max-h-full flex">
         <div className="md:w-[100%] mt-5 p-2">
           <div className="grid grid-cols-[300px_minmax(600px,_1fr)] gap-4 lg:gap-9 mr-8 ml-8">
-            <div className="bg-white border border-dashed border-black border-2">
+            <div className="bg-white border-dashed border-black border-2">
               <span>Filters</span>
             </div>
             <div className="grid grid-cols-[auto_auto] gap-5 md:grid-cols-[auto_auto_auto_auto] text-center">
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl flex flex-col">
-                <img
-                  src="https://www.segutecnica.com/images/000000000001757037365remera-naranja-segutecnica.png"
-                  alt="products"
-                  className="w-[200px] h-[200px] m-auto mt-[10px] mb-0"
-                />
-                <span className="fotn-bold text-lg mt-[20px]">
-                  Remera Naranja
-                </span>
-                <span>
-                  <b>7.999$</b>
-                </span>
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
-              <div className="bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto hover:shadow-2xl">
-                CARD
-              </div>
+              {
+                products?.map((product: IProduct) => (
+                  <div key={product._id} className="bg-white w-[15rem] h-[22rem] m-auto hover:shadow-2xl flex flex-col shadow-lg text-left relative">
+                    <img
+                      src={product.image}
+                      alt="products"
+                      className="w-[100%] h-[200px] m-auto mt-0 mb-0"
+                    />
+                    <span className="font-semibold text-base mt-[20px] ml-3">
+                      {product.name}
+                    </span>
+                    <span className="font-bold text-2xl ml-3">
+                      {product.price}$
+                    </span>
+                    <button className="absolute bottom-[325px] left-[210px]">
+                      <BsSuitHeartFill className="text-red-500" size="1.2rem"/>
+                    </button>
+                    <button className="bg-black text-center text-white m-auto mt-[35px] flex items-center justify-center p-1 w-[80%]">
+                      <span>Add to cart</span>
+                      <FaShoppingCart className="ml-2"/>
+                    </button>
+                  </div>
+                ))
+              }
             </div>
-            {/* <div className='grid grid-cols-[250px_250px_250px] gap-10 md:grid-cols-[auto_auto_auto_auto]'>
-                <div className='bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto'>CARD</div>
-                <div className='bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto'>CARD</div>
-                <div className='bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto'>CARD</div>
-                <div className='bg-white border border-dashed border-black border-2 w-[15rem] h-[22rem] m-auto'>CARD</div>
-              </div> */}
           </div>
         </div>
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find();
+  await db.disconnect();
+  return {
+    props: {
+      products:JSON.parse(JSON.stringify(products)),
+    }
+  }
 }
