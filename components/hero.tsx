4517b4a1a4fe354a2c551c7cafe1a6c2a8d1d800
FrancoG21 <fgimenez029@gmail.com>
2023-05-10 18:25:47 -0300

    feat: add hero and footer

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Hero() {
@@ -5,8 +6,10 @@ export default function Hero() {
     <div className="bg-black w-full md:h-[25rem] h-[16rem] bg-[url('https://stylecaster.com/wp-content/uploads/2022/12/mensfashiontrends.png')] bg-cover bg-no-repeat bg-center bg-origin-padding">
       <div className="md:w-[235px] w-[235px] m-auto">
         <div className="flex flex-row md:justify-between justify-between items-center md:w-[100%] md:mt-[280px] mt-[150px]">
-          <button className="bg-white text-black p-2 font-semibold">SHOP WOMEN</button>
-          <button className="bg-white text-black p-2 font-semibold">SHOP MEN</button>
+          <Link href="/products">
+            <button className="bg-white text-black p-2 font-semibold mr-1">SHOP WOMEN</button>
+            <button className="bg-white text-black p-2 font-semibold ml-1">SHOP MEN</button>
+          </Link>
         </div>
         <div className="justify-center m-auto items-center md:mt-[10px] mt-[10px]">
           <button className="bg-white text-black p-2 font-semibold md:w-[100%] w-[100%]">VIEW ALL PRODUCTS</button>
