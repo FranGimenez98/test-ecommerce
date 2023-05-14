661df7e7009daca897d0a264455b45584ae802d1
FrancoG21 <fgimenez029@gmail.com>
2023-05-14 13:50:08 -0300

    feat: navbar and responsive

---
+++ lib/useToggleMenu.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef, RefObject } from "react";
+
+export default function useDropdownMenu<T extends HTMLElement>(
+  initialState: boolean
+) {
+  const [showMenu, setShowMenu] = useState(initialState);
+  const [showProducts, setShowProducts] = useState(initialState);
+  const [showMenuMobile, setShowMenuMobile] = useState(initialState);
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setShowMenu(false);
+        setShowProducts(false);
+        setShowMenuMobile(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  function toggleProducts() {
+    setShowProducts(!showProducts);
+  }
+
+  function toggleMenuMobile() {
+    setShowMenuMobile(!showMenuMobile);
+  }
+
+  return { showMenu, showProducts, showMenuMobile, toggleMenu, toggleProducts, toggleMenuMobile, ref };
+}
